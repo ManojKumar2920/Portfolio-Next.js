8530095c9db252606d718058906e58c93cb7fa47
Manoj Kumar <manojkumararumainathan@gmail.com>
2025-06-22 17:05:34 +0530

    Update - 22/06/25 - deploy err fix

--- components/Thank.jsx
+++ components/Thank.jsx
@@ -8,7 +8,7 @@ const Thank = () => {
         <div className=' bg-white border-slate-200 rounded-lg shadow-sm border dark:border-slate-800 dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-800 odd:rotate-1 even:-rotate-1 hover:rotate-0 transition-transform duration-700 hover:duration-100 ease-in-out p-5'>
             <p className=' text-gray-500 text-[10px]'>&#128075; Thanks for visiting my site!</p>
             <h1 className=' font-bold text-sm py-2'>Do you have project idea ?</h1>
-            <Link target='_blank' href={'https://cal.com/manojkumar20/ship-now'} className=' text-purple-heart-500 text-sm flex items-center gap-2'>Let's Talk <Arrow /></Link>
+            <Link target='_blank' href={'https://cal.com/manojkumar20/ship-now'} className=' text-purple-heart-500 text-sm flex items-center gap-2'>Let&apos;s Talk <Arrow /></Link>
         </div>
     </div>
   )
